614bc2514dc0afe619db1015d339eb9166fa4d05
jaealways <71856506+jaealways@users.noreply.github.com>
2024-01-13 16:45:45 +0900

    Time: 203 ms (34.67%), Space: 18.1 MB (5.24%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -2,6 +2,7 @@
 s: dict 추가해서 count
 
 t에서 s dict 제거하면서 없을 때마다 count+=1
+s.length==t.length이므로 loop 한번만 돌게?
 
 """
 
@@ -11,16 +12,14 @@ class Solution:
         for i in range(len(s)):
             if s[i] not in dict_s:
                 dict_s[s[i]]=0
+            if t[i] not in dict_s:
+                dict_s[t[i]]=0
             dict_s[s[i]]+=1
-        
+            dict_s[t[i]]-=1
         answer=0
-        for i in range(len(t)):
-            if t[i] in dict_s:
-                dict_s[t[i]]-=1
-                if dict_s[t[i]]==0:
-                    del dict_s[t[i]]
-            else:
-                answer+=1
+        for key,val in dict_s.items():
+            if val>0:
+                answer+=val
         return answer
         
         
