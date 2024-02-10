b3b9be0d657ce976c5e11410a5f67e65c32d1835
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-02-10 22:07:35 +0900

    Time: 75 ms (84.75%), Space: 16.5 MB (72.69%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,28 @@
+"""
+같은 str여도 idx 다르면 다르게 취급
+
+pal1: i-1,i=1로 비교
+pal2: i,i+1 -> i-1,i+2 등으로 loop 돌면서 둘 다르면 stop
+
+두 idx 같을 때마다 answer+=1
+
+"""
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n,answer=len(s),0
+        def pal(l,r,answer):
+            while 0<=l and r<n:
+                if s[l]==s[r]:
+                    answer+=1
+                    l-=1
+                    r+=1
+                else:
+                    break
+            return answer
+        
+        for i in range(n):
+            answer=pal(i,i,answer)
+            answer=pal(i,i+1,answer)
+        return answer
+            
