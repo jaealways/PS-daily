70e4fa4adc206e46951076f581461aa4194d1c10
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-02-12 22:23:03 +0900

    Time: 132 ms (77.26%), Space: 18.1 MB (63.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,17 +2,16 @@
 O(1) space로 풀기
 2,2,2,1,1,1, 다음에 1오냐 2오냐에 따라 정답 달라짐
 
-dict로 채우면서, 남은 것 다 더해도 안되는 해 떨구기
-
+하나는 과반수 보장됨 -> linear하게 가다가 같은 값 나오면 +, 아니면 -
+차피 과반수면 나머지 다 합한 것보다 더 큼
 
 """
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict_num={}
-        n=len(nums)
-        for i in range(n):
-            dict_num[nums[i]]=dict_num.get(nums[i],0)+1
-            if dict_num[nums[i]]>n//2:
-                break
-        return nums[i]
+        key,val=0,0
+        for i in range(len(nums)):
+            if val==0:
+                key=nums[i]
+            val+=(1 if key==nums[i] else -1)
+        return key
