57e6a9bf31ac6524d55260707abc1dd797c80fc6
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-06-29 17:03:21 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,27 +1,29 @@
 """
-노래 수록 기준: 장르(최대 2개), 노래, 고유번호 낮은
+장르별 많이 재생된노래 두 개 -> 베스트앨범
+1. 각 장르별 play 합 구하기
+2. 각 장르에서 가장 많이 재생된 순으로(재생수 같으면 번호 낮은 순)
+3. 각 장르 * 2로 나열하기
+* 장르에 속한 곡이 하나면 하나만...
 
-각 장르별 재생횟수
-dict: 장르-번호
-(plays, genre) 만들어서 sort
-genre 내의 (plays, idx) 만들어서 plays, idx 순으로 sort [:2]
-answer에 append
+(num,play,genre) 순으로 만들기, play 순으로 정렬
+dict_genre에서 두 개 이상이면 pass, 아니면 num 추가
+
 """
 
 def solution(genres, plays):
-    dict_genre,count_genre={},{}
-    answer=[]
+    dict_genre_total={}
+    answer,list_total=[],[]
     for i in range(len(genres)):
-        if genres[i] not in dict_genre:
-            dict_genre[genres[i]]=[]
-        dict_genre[genres[i]].append([i,plays[i]])
-    for idx,val in dict_genre.items():
-        count_genre[idx]=sum([x[1] for x in val])
-    array_genre=list(count_genre.items())
-    array_genre.sort(key=lambda x:-x[1])
-    for i in range(len(array_genre)):
-        cat=array_genre[i][0]
-        tmp=dict_genre[cat]
-        tmp.sort(key=lambda x: (-x[1],x[0]))
-        answer.extend([x[0] for x in tmp[:2]])
+        g,p=genres[i],plays[i]
+        dict_genre_total[g]=dict_genre_total.get(g,0)+p
+    for i in range(len(genres)):
+        g,p=genres[i],plays[i]
+        list_total.append((i,p,g,dict_genre_total[g]))
+    list_total.sort(key=lambda x: (-x[3],-x[1],x[0]))
+    count_genre={}
+    for i,p,g,t in list_total:
+        count_genre[g]=count_genre.get(g,0)+1
+        if count_genre[g]>2:
+            continue
+        answer.append(i)
     return answer
