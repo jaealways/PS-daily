db8849d57d27ade512c7ad6240f4896ef709c3d1
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-06-29 16:24:38 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,20 +1,19 @@
 """
-category 별 dict 분류
+각 종류의 갯수를 dict 형태로 저장 후 val 값으로 list 만들기
+[4,5,6,2]
 
-headgear: 2개
-eyewear: 1개
-face: 3개
+전체 경우의 수는 각 카테고리의 것을 선택하느냐 마느냐
+(4+1(선택 안하는 옵션))*(5+1)*(6+1)*(2+1)-1(전체 선택하지 않는 경우)
 
-각 cat+1 해서 곱 -1
+...
+
 """
 
-def solution(clothes):
-    dict_clo={}
-    answer=1
-    for name,cat in clothes:
-        if cat not in dict_clo:
-            dict_clo[cat]=[]
-        dict_clo[cat].append(name)
+def solution(clothes):    
+    answer = 1
+    dict_clo,list_clo={},[]
+    for val1,val2 in clothes:
+        dict_clo[val2]=dict_clo.get(val2,0)+1
     for key,val in dict_clo.items():
-        answer*=len(val)+1
+        answer*=(val+1)
     return answer-1
