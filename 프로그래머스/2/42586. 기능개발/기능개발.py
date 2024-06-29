005ae40522aceb373bff0be0ba206a4740328f93
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-06-29 20:36:09 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,26 @@
 """
-앞 기능이 우선 배포
-
-dday=[7,3,9] -> 남은 일자 list로 만들기
-
-loop 돌다가, 맨 앞보다 커지면 stop -> 그 동안 쌓인 갯수 append
-
+1. 각 작업별 걸리는 일수 list 만들기
+(100-작업, 나머지 있으면 반올림 없으면 몫 사용)
+ex) [93,30,55] -> [7,3,9]
+2. 위의 list for문 돌면서
+이전 최대값 < 새 값: 이전까지 쌓아온 tmp list에 추가, 최대값 초기화
+최대값 > 새 값: tmp+=1
+마지막 tmp 추가
 """
+
+import math
+
 def solution(progresses, speeds):
-    n=len(progresses)
-    dday,answer=[],[]
-    for i in range(n):
-        d,v=divmod((100-progresses[i]),speeds[i])
-        if v>0:
-            d+=1
-        dday.append(d)
-    maxN,count=dday[0],1
-    for i in range(1,n):
-        if dday[i]>maxN:
-            answer.append(count)
-            maxN,count=dday[i],1
+    answer=[]
+    list_day=[math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
+    maxv,c=list_day[0],1
+    # 좀 더 단순하고 직관적인 구조 생각해보기
+    for d in list_day[1:]:
+        if d<=maxv:
+            c+=1
         else:
-            count+=1
+            answer.append(c)
+            maxv,c=d,1
     else:
-        answer.append(count)    
+        answer.append(c)
     return answer
