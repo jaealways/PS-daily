3e1b48e04e24e576dd28e7848d61a8151a607560
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-06-30 20:51:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,32 +1,38 @@
 """
-passed, bridge, wait = [],[0,0],[]
-bridgesum + wait[0] < 최대 -> wait popleft
-아니면 popleft 후 (passed에 append) append(wait[0])
+truck_weight popleft -> bridge popleft -> after_bridge
+n=len(truck_weight)
 
-len(passed)==n: 종료
+popleft -> next truck()
+if next_truck not -1:
+    popleft 하지말기
+if next_truck + bridge < weight:
+    next_truck=-1
 
+
+if after_bridge==n이면 stop
+    
 """
 
 def solution(bridge_length, weight, truck_weights):
     from collections import deque
     
-    answer,n,bridgesum=0,len(truck_weights),0
-    passed,bridge,wait=deque([]),deque([0 for _ in range(bridge_length)]),deque(truck_weights)
+    truck_weights=deque(truck_weights)
+    answer,n,next_truck,bridge_sum=0,len(truck_weights),0,0
+    after_bridge,bridge=[],deque([0 for _ in range(bridge_length)])
     
-    while len(passed)<n:
-        answer+=1
-        p=0
-        if wait:
-            p=wait[0]
-        e=bridge.popleft()
-        if e>0:
-            passed.append(e)
-        bridgesum-=e
-        if bridgesum+p<=weight:
-            bridgesum+=p
-            bridge.append(p)
-            if wait:
-                wait.popleft()
+    while len(after_bridge)<n:
+        if next_truck==0 and len(truck_weights):
+            next_truck=truck_weights.popleft()
+        t=bridge.popleft()
+        bridge_sum-=t
+        if t>0:
+            after_bridge.append(t)
+        if next_truck+bridge_sum<=weight:
+            bridge_sum+=next_truck
+            bridge.append(next_truck)
+            next_truck=0
         else:
             bridge.append(0)
+        answer+=1
+    
     return answer
