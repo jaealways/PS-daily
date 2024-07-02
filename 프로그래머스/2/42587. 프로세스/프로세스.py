5bfc9d802fdaf4848a075f18708610ddd6a9fe8d
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-07-02 14:49:20 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,19 +1,28 @@
 """
-계속 loop 돌다가, 
+100개 O(n^4)까지는 가능
+(p,i) <- 쌍으로 만들기
+시작: (10,-1)
+
+1) 같은 p 중에 i가 뒤에 있는 값들만 가져오기 -> 정렬해서 point 갱신
+2) 같은 p 중에 i가 앞에 있는 값들 가져오기 -> 정렬해서 point 갱신
+
+for loop 사용
 
 """
 
+from collections import deque
+
 def solution(priorities, location):
+    queue = deque([(priority, i) for i, priority in enumerate(priorities)])
     answer = 0
-    place=priorities.index(max(priorities))
-    while True:
-        value=max(priorities)
-        if priorities[place]==value:
-            priorities[place]=0
-            answer+=1
-            if place==location:
-                break
-        place+=1
-        if place>=len(priorities):
-            place=0
+
+    while queue:
+        current = queue.popleft()
+        if any(current[0] < item[0] for item in queue):
+            queue.append(current)
+        else:
+            answer += 1
+            if current[1] == location:
+                return answer
+
     return answer
